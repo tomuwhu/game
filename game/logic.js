018d239f6837df902a927fe8257eeb3f773f5a6b
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-03-15 18:39:49 +0100

    .

--- game/logic.js
+++ game/logic.js
@@ -6,21 +6,18 @@ window.addEventListener("load", e => {
             .join("") + "</div>";
     document.getElementById("bsx").innerHTML = s;
 });
-function f(e) {
-    x=e.target.getAttribute("src");
-    v=x.split(".")[0].substring(5);
-    document.getElementById("b1").innerHTML = `<a href="game/"><img src="${x}"></a><br>`+nevek[v]
-}
-
 xm = window.innerWidth;
 ym = window.innerHeight;
 tx = xm / 2 - 50;
 ty = ym - 150;
 xv = 0;
 yv = 0;
+ci = 0;
 speed = 20;
 function start(x) {
+    document.getElementById("body").addEventListener("keydown", f);
     document.getElementById("bsx").style.display = "none";
+    document.getElementById("o1").style.display = "inline-block";
     tank = document.getElementById("tank");
     tank.innerHTML = `<img src="../pic/o${x}.pgj.png">`;
     tank.style.display = "inline-block";
@@ -35,6 +32,29 @@ function start(x) {
         tank.style.top = `${ty}px`;
         tank.style.left = `${tx}px`;
     }, 200);
+    l = setInterval(() => {
+        document.querySelectorAll(".golyo").forEach( v => {
+            x1 = v;
+            x2 = document.getElementById("o1");
+            if (coll(x1, x2)) {
+                clearInterval(l);
+                setTimeout(() => {
+                    x1.style.display = "none";
+                    x2.style.display = "none";
+                }, 50);
+            }
+        });
+    }, 10);
+}
+function coll(el1, el2) {
+    var rect1 = el1.getBoundingClientRect();
+    var rect2 = el2.getBoundingClientRect();
+    return !(
+      rect1.top > rect2.bottom ||
+      rect1.right < rect2.left ||
+      rect1.bottom < rect2.top ||
+      rect1.left > rect2.right
+    );
 }
 function f(e) {
     switch (e.key) {
@@ -58,22 +78,25 @@ function f(e) {
             xv = 0;
             yv = 0;
             st = document.createElement('style');
-            st.type = "text/css"
-            tn = document.createTextNode(`@keyframes g1 {
+            st.type = "text/css";
+            tn = document.createTextNode(`@keyframes g${ci} {
                 0% {
                     left: ${tx + 45}px;
                     top: ${ty}px;
                 }
                 100% {
-                    left: ${tx + 45}px;
+                    left: ${tx + 45 + Math.round(Math.random()*200-50)}px;
                     top: -20px;
                 }
-            }`)
-            st.appendChild(tn)
+            }`);
+            st.appendChild(tn);
             g = document.createElement("div");
-            g.id = `g1`;
-            document.getElementById("body").appendChild(st)
-            document.getElementById("body").appendChild(g)
+            g.className = "golyo";
+            g.style = `animation-name: g${ci};`
+            ci++;
+            document.getElementById("body").appendChild(st);
+            document.getElementById("body").appendChild(g);
             break;
+
     }
 }
